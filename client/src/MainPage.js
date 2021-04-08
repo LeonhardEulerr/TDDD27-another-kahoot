4112bede8687e7c79e23851df7dc4d13020332ad
unknown <dawab699@student.liu.se>
2021-04-08 17:25:56 +0200

    test: experiment with layout

--- client/src/MainPage.js
+++ client/src/MainPage.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { Button, Container, CssBaseline, Grid } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Divider,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
+  container: {
+    padding: '0em',
+    minWidth: '100vw',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
   button: {
     margin: 2,
   },
+  topPaper: {},
 });
 
 export default function MainPage() {
   const classes = useStyles();
   return (
-    <Container style={{ backgroundColor: '#123111' }}>
-      {/* <CssBaseline /> */}
+    <Container className={classes.container}>
+      <CssBaseline />
+      <Box
+        style={{
+          minHeight: '10vh',
+          display: 'block',
+          background: '#CFCFCF',
+          padding: '0em',
+        }}
+      >
+        Dawid
+        <Divider variant="middle" />
+      </Box>
       <Grid
         container
         direction="row"
         alignItems="center"
         justify="center"
-        style={{ minHeight: '100vh' }}
+        style={{ minHeight: '90vh', backgroundColor: '#DFDFDF' }}
       >
         <Button className={classes.button} color="primary" variant="contained">
           Create
