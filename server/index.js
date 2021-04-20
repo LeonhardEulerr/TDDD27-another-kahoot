d4606fad9978f4624fa219dbe8b445c8f9a204ec
Dawid Abucewicz <dawab699@student.liu.se>
2021-04-20 12:22:46 +0200

    feat: add database and connect to it

--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully!');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
