b6db3a22c2f6b753c2a4f85b25a74672d51386de
Dawid Abucewicz <dawab699@student.liu.se>
2021-04-20 22:22:11 +0200

    feat: add mongodb connection

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+const AuthRoute = require('./routes/auth');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -23,3 +29,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.use('/api', AuthRoute);
