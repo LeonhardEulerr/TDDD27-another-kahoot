0733703365d3159a36f4048bf069560260a18d0b
Dawid Abucewicz <dawab699@student.liu.se>
2021-05-10 23:09:05 +0200

    feat: add socket event for host and participants joining

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,6 +1,14 @@
 const socketio = require('socket.io');
 const http = require('http');
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUserByNameAndPin,
+  getUsersInQuiz,
+} = require('./users');
+
 module.exports.server = function (app) {
   const server = http.createServer(app);
   const io = socketio(server);
@@ -8,18 +16,44 @@ module.exports.server = function (app) {
   io.on('connection', (socket) => {
     console.log('New user connected', socket.id);
 
-    socket.on('lala', ({ lala }, callback) => {
-      console.log(lala);
-      socket.emit('message', { message: 'answer from the server' });
-      //callback();
+    socket.on('joinHost', ({ pin }, callback) => {
+      const { error, user } = addUser({
+        id: socket.id,
+        name: 'host',
+        pin: pin,
+      });
+
+      if (error) {
+        return callback(error);
+      }
+
+      socket.join(user.pin);
+      callback({ message: 'SUCCESS' });
     });
 
-    socket.on('message', ({ message }, _callback) => {
+    socket.on('participantJoin', ({ pin, name }, callback) => {
+      const { error, user } = addUser({
+        id: socket.id,
+        name,
+        pin,
+      });
+
+      if (error) {
+        return callback(error);
+      }
+
+      socket.join(user.pin);
+      const host = getUserByNameAndPin('host', pin);
+      io.to(host.id).emit('newUser', { name });
+    });
+
+    socket.on('message', ({ message }) => {
       console.log(message);
     });
 
     socket.on('disconnect', () => {
       console.log('User disconnected');
+      removeUser(socket.id);
     });
   });
   return server;
