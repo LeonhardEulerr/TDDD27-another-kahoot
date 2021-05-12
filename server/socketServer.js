a072d5383c734e31eaf3e4bf4efb1721b4f0e4f9
Dawid Abucewicz <dawab699@student.liu.se>
2021-05-12 12:35:51 +0200

    feat: add quiz events in socket server

--- server/socketServer.js
+++ server/socketServer.js
@@ -9,6 +9,8 @@ const {
   getUsersInQuiz,
 } = require('./users');
 
+const { addQuiz, removeQuiz, getNextQuestion } = require('./quizzes');
+
 module.exports.server = function (app) {
   const server = http.createServer(app);
   const io = socketio(server);
@@ -31,7 +33,7 @@ module.exports.server = function (app) {
       callback({ message: 'SUCCESS' });
     });
 
-    socket.on('participantJoin', ({ pin, name }, callback) => {
+    socket.on('joinParticipant', ({ pin, name }, callback) => {
       const { error, user } = addUser({
         id: socket.id,
         name,
@@ -39,12 +41,38 @@ module.exports.server = function (app) {
       });
 
       if (error) {
-        return callback(error);
+        return callback({ error });
       }
 
       socket.join(user.pin);
       const host = getUserByNameAndPin('host', pin);
       io.to(host.id).emit('newUser', { name });
+      callback({ success: true });
+    });
+
+    socket.on('addQuiz', ({ pin, quiz }, callback) => {
+      const { error } = addQuiz({ pin, quiz });
+
+      if (error) {
+        callback({ error: 'Quiz could not be started' });
+      }
+
+      callback({ success: true });
+    });
+
+    socket.on('loadNextQuestionView', ({ pin }, callback) => {
+      socket.broadcast.to(pin).emit('loadNextQuestionView', {});
+      callback({ success: true });
+    });
+
+    socket.on('getNextQuestion', ({ pin }, callback) => {
+      const question = getNextQuestion(pin);
+      const user = getUser(socket.id);
+      if (question && user) {
+        callback({ question, name: user.name });
+      }
+
+      callback({ error: 'Question or user could not be fethced' });
     });
 
     socket.on('message', ({ message }) => {
@@ -53,6 +81,14 @@ module.exports.server = function (app) {
 
     socket.on('disconnect', () => {
       console.log('User disconnected');
+      const user = getUser(socket.id);
+
+      if (user && user.name === 'host') {
+        console.log('removing host');
+        removeQuiz(user.pin);
+      }
+
+      // remove all the users from the started quiz and redirect them to start page
       removeUser(socket.id);
     });
   });
