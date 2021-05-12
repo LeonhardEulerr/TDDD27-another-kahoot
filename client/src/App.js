f8bbf62dd14f78e858fa9427221c97778acd1557
Dawid Abucewicz <dawab699@student.liu.se>
2021-05-12 12:57:11 +0200

    fix: use NextQuestion View and LobbyParticipants View

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import JoinPage from './JoinPage';
 import MainPage from './MainPage';
-import Lobby from './Lobby';
+import LobbyParticipant from './LobbyParticipant';
 import ProtectedRoute from './ProtectedRoute';
 import CreateQuizPage from './CreateQuizPage';
 import ProfilePage from './ProfilePage';
@@ -15,9 +15,22 @@ import { QuizContext } from './Contexts/QuizContext';
 import HostLobby from './HostLobby';
 
 import socket from './socketCfg';
+import NextQuestion from './NextQuestion';
+import QuestionPage from './QuestionPage';
 
 function App() {
   const [pin, setPin] = useState('');
+  const [quiz, setQuiz] = useState({});
+  const [indexQuestion, setIndexQuestion] = useState(0);
+
+  const quizStates = {
+    pin,
+    setPin,
+    quiz,
+    setQuiz,
+    indexQuestion,
+    setIndexQuestion,
+  };
 
   useEffect(() => {
     socket.on('message', (data) => {
@@ -31,7 +44,7 @@ function App() {
   }, []);
   return (
     <SocketContext.Provider value={{ socket }}>
-      <QuizContext.Provider value={{ pin, setPin }}>
+      <QuizContext.Provider value={quizStates}>
         <Router>
           <Switch>
             <Route exact path="/">
@@ -41,12 +54,16 @@ function App() {
               <JoinPage />
             </Route>
             <Route exact path="/lobby">
-              <Lobby />
+              <LobbyParticipant />
+            </Route>
+            <Route exact path="/nextQuestion">
+              <NextQuestion />
             </Route>
             <ProtectedRoute path="/create/:id" component={CreateQuizPage} />
             <ProtectedRoute path="/create" component={CreateQuizPage} />
             <ProtectedRoute path="/profile" component={ProfilePage} />
             <ProtectedRoute path="/hostLobby" component={HostLobby} />
+            <ProtectedRoute path="/question" component={QuestionPage} />
           </Switch>
         </Router>
       </QuizContext.Provider>
