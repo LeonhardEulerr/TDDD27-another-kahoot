d8b05c8d4f799f554e5621addd8ebeb217d23757
Dawid Abucewicz <dawab699@student.liu.se>
2021-05-18 19:42:45 +0200

    feat: retreive pin and username on reload

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import ProtectedRoute from './ProtectedRoute';
 import CreateQuizPage from './CreateQuizPage';
 import ProfilePage from './ProfilePage';
 
+import history from './history';
+
 // Contexts
 import { SocketContext } from './Contexts/SocketContext';
 import { QuizContext } from './Contexts/QuizContext';
@@ -37,51 +39,72 @@ function App() {
   };
 
   useEffect(() => {
-    socket.on('message', (data) => {
-      console.log(data);
-    });
+    retrieveLocalStorage();
+
+    if (localStorage.getItem('name')) {
+      if (localStorage.getItem('name') == 'host') {
+        history.replace('/');
+      }
+    }
+
+    if (localStorage.getItem('pin') && localStorage.getItem('name')) {
+      socket.emit('rejoin', {
+        pin: localStorage.getItem('pin'),
+        name: localStorage.getItem('name'),
+      });
+    }
 
     return () => {
       socket.close();
       socket.off();
     };
   }, []);
+
+  const retrieveLocalStorage = () => {
+    if (localStorage.getItem('pin')) {
+      console.log(localStorage.getItem('pin'));
+      setPin(localStorage.getItem('pin'));
+    }
+
+    if (localStorage.getItem('index')) {
+      setIndexQuestion(localStorage.getItem('index'));
+    }
+  };
+
   return (
     <SocketContext.Provider value={{ socket }}>
       <QuizContext.Provider value={quizStates}>
-        <Router>
-          <Switch>
-            <Route exact path="/">
-              <MainPage />
-            </Route>
-            <Route exact path="/join">
-              <JoinPage />
-            </Route>
-            <Route exact path="/lobby">
-              <LobbyParticipant />
-            </Route>
-            <Route exact path="/nextQuestion">
-              <NextQuestion />
-            </Route>
-            <Route exact path="/wait">
-              <WaitForAnswerParticipantView />
-            </Route>
-            <Route exact path="/answer">
-              <ShowAnswerParticipant />
-            </Route>
-            <Route exact path="/register">
-              <RegisterPage />
-            </Route>
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route exact path="/join">
+            <JoinPage />
+          </Route>
+          <Route exact path="/lobby">
+            <LobbyParticipant />
+          </Route>
+          <Route exact path="/nextQuestion">
+            <NextQuestion />
+          </Route>
+          <Route exact path="/wait">
+            <WaitForAnswerParticipantView />
+          </Route>
+          <Route exact path="/answer">
+            <ShowAnswerParticipant />
+          </Route>
+          <Route exact path="/register">
+            <RegisterPage />
+          </Route>
 
-            <ProtectedRoute path="/create/:id" component={CreateQuizPage} />
-            <ProtectedRoute path="/create" component={CreateQuizPage} />
-            <ProtectedRoute path="/profile" component={ProfilePage} />
-            <ProtectedRoute path="/hostLobby" component={HostLobby} />
-            <ProtectedRoute path="/question" component={QuestionPage} />
-            <ProtectedRoute path="/stats" component={StatsPage} />
-            <ProtectedRoute path="/endquiz" component={FinishQuiz} />
-          </Switch>
-        </Router>
+          <ProtectedRoute path="/create/:id" component={CreateQuizPage} />
+          <ProtectedRoute path="/create" component={CreateQuizPage} />
+          <ProtectedRoute path="/profile" component={ProfilePage} />
+          <ProtectedRoute path="/hostLobby" component={HostLobby} />
+          <ProtectedRoute path="/question" component={QuestionPage} />
+          <ProtectedRoute path="/stats" component={StatsPage} />
+          <ProtectedRoute path="/endquiz" component={FinishQuiz} />
+        </Switch>
       </QuizContext.Provider>
     </SocketContext.Provider>
   );
